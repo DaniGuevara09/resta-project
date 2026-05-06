03afe3ebe06766901fd5445e7a856d5a8d1c07e6
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2026-05-06 08:34:25 -0500

    configuration of enviroments and angular.js file

---
+++ resta-frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    name: 'sede-central',
+    apiProducts: 'http://localhost:3001/api',
+    apiOrders: 'http://localhost:3002/api',
+    apiBilling: 'http://localhost:3003/api',
+    keycloakUrl: 'http://192.168.1.14:8082/realms/Restaurant/protocol/openid-connect/token',
+    theme: {
+        primaryColor: '#2600ffff',
+        secondaryColor: '#000000',
+        logo: 'assets/logos/logoCentral.png',
+        background: '#f4f4f9'
+    }
+};
---
+++ resta-frontend/src/environments/environments.sucursal.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    name: 'sede-sucursal',
+    apiProducts: 'http://localhost:3001/api',
+    apiOrders: 'http://localhost:3002/api',
+    apiBilling: 'http://localhost:3003/api',
+    keycloakUrl: 'http://192.168.1.14:8082/realms/Restaurant/protocol/openid-connect/token',
+    theme: {
+        primaryColor: '#D50000',
+        secondaryColor: '#FFFFFF',
+        logo: 'assets/logos/logoSucursal.png',
+        background: '#fff5f5'
+    }
+};
