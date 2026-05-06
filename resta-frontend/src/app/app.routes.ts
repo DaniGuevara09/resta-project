bb4cc585d78efe43cc2a09e41f1feda5d2795f63
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2026-05-06 09:59:47 -0500

    path configuration

--- resta-frontend/src/app/app.routes.ts
+++ resta-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AutenticacionComponent } from './components/autentication/autentication';
+import { ListProducts } from './components/list-products/list-products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AutenticacionComponent },
+    { path: 'productos', component: ListProducts }
+];
