a442706acb8bb80041431682922a7b53f4b13646
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2026-05-12 21:40:52 -0500

    succesfuly integration between backend-python with keycloak

--- backend-python/product-service/app.py
+++ backend-python/product-service/app.py
@@ -74,4 +74,4 @@ if __name__ == '__main__':
         db.session.commit()
         db.create_all() 
     
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
