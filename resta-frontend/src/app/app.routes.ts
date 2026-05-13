6b64e018436105e3a06da40600ca985045c67733
Andres Puentes <andres.puentes02@uptc.edu.co>
2026-05-13 08:13:55 -0500

    base inicial de front conexion con keycloack y login

--- resta-frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { AutenticacionComponent } from './components/autentication/autentication';
-import { ListProducts } from './components/list-products/list-products';
-
-export const routes: Routes = [
-    { path: '', component: AutenticacionComponent },
-    { path: 'productos', component: ListProducts }
-];
